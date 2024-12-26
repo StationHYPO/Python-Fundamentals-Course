5cfc46bcf29df120ef0d9b65af89796b330cb6a0
StationHYPO <christopher.padilla@laverne.edu>
2024-12-25 22:46:50 -0800

    Removed all files to recreate them from scratch.

--- module_01_getting_started/temperature_converter.py
+++
@@ -1,9 +0,0 @@
-#A simple calculator which will convert F to C.
-
-f_temp = float(input("Enter the temperature in F: "))
-
-#Convert F to C.
-c_temp = (f_temp - 32) * 5/9
-
-#Output the new temp to the user.
-print(f"{f_temp:.2f} degrees Farenheit is {c_temp:.2f} degrees Celcius.")
